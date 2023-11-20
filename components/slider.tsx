c50da31dafd79637e5e084899d3aac88c4ae9a79
Muhammad Yasir <imyasir79@yahoo.com>
2023-11-20 23:03:56 +0500

    Removed Carousel from Hero Slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -9,12 +9,9 @@ const Slider = async () => {
 
   return (
 
-<div className="mr-28 my-40 ml-10  w-full overflow-x-auto">
-  <div className="flex animate-carousel gap-4">
-
+<div className="mr-28 my-40 ml-10 flex">
     {res.map((res: any, index: number) => (
       <div key={index}>
-        
         <div className='p-1 '>
             <div>
               <Image src={res.imageURL} alt={res.key} width={500} height={500} />
@@ -22,16 +19,15 @@ const Slider = async () => {
             <div className='text-center my-4'>
               <p className='my-4'>{res.value}</p>
               <p className='my-4'>Rs.5000</p>
-              <button className='my-4 bg-pink-500 rounded-md py-4 m-4 '>
+              <div className='my-4 bg-pink-500 rounded-md py-4 m-4 '>
                 Buy Product
-              </button>
+              </div>
               
             </div>
           </div>
         
       </div>
     ))}
-    </div>
   </div>
 
   );
